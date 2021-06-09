f62786c39f2d2f96c66cdb3ff37ef1a50c2c4758
Aviram Hassan <41201924+aviramha@users.noreply.github.com>
2021-06-09 18:10:17 +0300

    Add release action
    * add release action

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # Changelog
-## TBD
+## Next Version
+## 0.1.0 - 9/6/2021
 
 First version, changed orjson to ormsgpack.
---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="ormsgpack",
+    url="https://github.com/aviramha/ormsgpack",
+)
