41b3b648e5190c7e1c006ca524c1720303a6fd6c
ijl <ijl@mailbox.org>
2021-06-19 00:51:04 -0500

    profile.sh for perf (#17)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,7 +3,7 @@
 set -eou pipefail
 
 autoflake --in-place --recursive --remove-all-unused-imports --ignore-init-module-imports .
-isort ./ormsgpack.pyi ./tests/*.py ./benchmarks/*.py
-black ./ormsgpack.pyi ./tests/*.py ./benchmarks/*.py
-mypy --ignore-missing-imports ./ormsgpack.pyi ./tests/*.py ./benchmarks/*.py
+isort ./ormsgpack.pyi ./tests/*.py ./benchmarks/*.py ./scripts/*.py
+black ./ormsgpack.pyi ./tests/*.py ./benchmarks/*.py ./scripts/*.py
+mypy --ignore-missing-imports ./ormsgpack.pyi ./tests/*.py ./benchmarks/*.py ./scripts/*.py
 cargo fmt
---
+++ scripts/profile.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+
+# usage: ./scripts/profile.sh benchmarks/samples/citm_catalog.mpack packb
+
+perf record -g --delay 250 ./scripts/bench_target.py "$@"
+perf report --percent-limit 0.1
+rm -f perf.data*
