9bd6e4fea7e8e788b0fd1755dc07c0f6a9b39be1
Aviram Hassan <aviram.hassan@biocatch.com>
2021-07-23 08:03:16 +0300

    Fix build.rs and add pyo3-build-config change minimum for fastcall to py3.8

--- build.rs
+++ build.rs
@@ -1,15 +1,3 @@
-use std::process::Command;
-
 fn main() {
-    let out = Command::new("python")
-        .args(&["-c", "import sys; print(sys.version_info[1])"])
-        .output()
-        .expect("python version did not print");
-    let version = String::from_utf8_lossy(&out.stdout)
-        .trim()
-        .parse::<u8>()
-        .expect("python version was not parsed");
-    for each in 6..version {
-        println!("cargo:rustc-cfg=python3{}", each);
-    }
+    pyo3_build_config::use_pyo3_cfgs();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -84,7 +84,7 @@ pub unsafe extern "C" fn PyInit_ormsgpack() -> *mut PyObject {
     let wrapped_packb: PyMethodDef;
     let wrapped_unpackb: PyMethodDef;
 
-    #[cfg(Py_3_7)]
+    #[cfg(Py_3_8)]
     {
         wrapped_packb = PyMethodDef {
             ml_name: "packb\0".as_ptr() as *const c_char,
@@ -96,7 +96,7 @@ pub unsafe extern "C" fn PyInit_ormsgpack() -> *mut PyObject {
         };
     }
 
-    #[cfg(not(Py_3_7))]
+    #[cfg(not(Py_3_8))]
     {
         wrapped_packb = PyMethodDef {
             ml_name: "packb\0".as_ptr() as *const c_char,
@@ -118,7 +118,7 @@ pub unsafe extern "C" fn PyInit_ormsgpack() -> *mut PyObject {
         )
     );
 
-    #[cfg(Py_3_7)]
+    #[cfg(Py_3_8)]
     {
         wrapped_unpackb = PyMethodDef {
             ml_name: "unpackb\0".as_ptr() as *const c_char,
@@ -130,7 +130,7 @@ pub unsafe extern "C" fn PyInit_ormsgpack() -> *mut PyObject {
         };
     }
 
-    #[cfg(not(Py_3_7))]
+    #[cfg(not(Py_3_8))]
     {
         wrapped_unpackb = PyMethodDef {
             ml_name: "unpackb\0".as_ptr() as *const c_char,
@@ -250,7 +250,7 @@ fn raise_packb_exception(msg: Cow<str>) -> *mut PyObject {
     std::ptr::null_mut()
 }
 
-#[cfg(Py_3_7)]
+#[cfg(Py_3_8)]
 #[no_mangle]
 pub unsafe extern "C" fn unpackb(
     _self: *mut PyObject,
@@ -307,7 +307,7 @@ pub unsafe extern "C" fn unpackb(
     }
 }
 
-#[cfg(not(Py_3_7))]
+#[cfg(not(Py_3_8))]
 #[no_mangle]
 pub unsafe extern "C" fn unpackb(
     _self: *mut PyObject,
@@ -369,7 +369,7 @@ pub unsafe extern "C" fn unpackb(
     }
 }
 
-#[cfg(Py_3_7)]
+#[cfg(Py_3_8)]
 #[no_mangle]
 pub unsafe extern "C" fn packb(
     _self: *mut PyObject,
@@ -437,7 +437,7 @@ pub unsafe extern "C" fn packb(
     }
 }
 
-#[cfg(not(Py_3_7))]
+#[cfg(not(Py_3_8))]
 #[no_mangle]
 pub unsafe extern "C" fn packb(
     _self: *mut PyObject,
