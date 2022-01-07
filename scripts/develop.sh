21e2d2519e47722453acab6a9242c9f0bf20239c
Aviram Hassan <aviramyhassan@gmail.com>
2022-01-07 22:13:49 +0200

    remove CPU flags from develop

--- scripts/develop.sh
+++ scripts/develop.sh
@@ -2,8 +2,6 @@
 
 rm -f target/wheels/*
 
-export RUSTFLAGS="-C target-cpu=k8"
-
 maturin build --no-sdist --manylinux off -i python3 --release "$@"
 
 pip install --force $(find target/wheels -name "*cp3*")
