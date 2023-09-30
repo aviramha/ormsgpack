866e232180771e7ee7585387834f91ae3bb83cd7
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2023-09-30 10:49:58 +0300

    Streamline serialization of dicts with non-str keys
    The serializer for dicts with non-str keys duplicates the code to
    serialize scalar types instead of using a PyObjectSerializer for keys,
    seemingly for speed reasons. However, based on the orjson benchmark
    for non-str keys, the code using a PyObjectSerializer is faster:
    
    Name (time in ms)           Min               Max              Mean            StdDev            Median               IQR            Outliers       OPS            Rounds  Iterations
    -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    ormsgpack(master)        1.3515 (1.0)      3.9225 (1.0)      1.3902 (1.0)      0.0977 (1.0)      1.3750 (1.0)      0.0173 (1.0)        73;362  719.3031 (1.0)        3456
    ormsgpack(head)          1.1721 (1.0)      2.8177 (1.0)      1.2137 (1.0)      0.1055 (1.0)      1.1941 (1.0)      0.0175 (1.0)       104;436  823.9116 (1.0)        4089
    
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

---
+++ benchmarks/bench_non_str_keys.py
@@ -0,0 +1,29 @@
+import datetime
+import random
+from time import mktime
+
+import msgpack
+import pytest
+
+import ormsgpack
+
+data = []
+for year in range(1920, 2020):
+    start = datetime.date(year, 1, 1)
+    array = [
+        (int(mktime((start + datetime.timedelta(days=i)).timetuple())), i + 1)
+        for i in range(0, 365)
+    ]
+    array.append(("other", 0))
+    random.shuffle(array)
+    data.append(dict(array))
+
+
+def test_msgpack_packb(benchmark):
+    benchmark.group = "non_str_keys"
+    benchmark(msgpack.packb, data)
+
+
+def test_ormsgpack_packb(benchmark):
+    benchmark.group = "non_str_keys"
+    benchmark(ormsgpack.packb, data, option=ormsgpack.OPT_NON_STR_KEYS)
