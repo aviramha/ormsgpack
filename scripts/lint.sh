25862e8a90fd5131d6568f9a13d87f394241afe2
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2024-02-24 10:32:48 +0200

    Switch to ruff
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,9 +2,8 @@
 
 set -eou pipefail
 
-autoflake --check --recursive --remove-all-unused-imports --ignore-init-module-imports .
-isort --check .
-black --check .
+ruff format --check .
+ruff check .
 mypy --ignore-missing-imports .
 cargo fmt --check
 cargo clippy -- -D warnings
