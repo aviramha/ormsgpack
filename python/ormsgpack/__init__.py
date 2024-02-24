2fe36070376fad77301a029c2eb0cec7e86b561c
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2024-02-24 10:32:48 +0200

    Switch to ruff
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- python/ormsgpack/__init__.py
+++ python/ormsgpack/__init__.py
@@ -1,7 +1,25 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from .ormsgpack import *
-from .ormsgpack import __version__
+from .ormsgpack import (
+    OPT_NAIVE_UTC,
+    OPT_NON_STR_KEYS,
+    OPT_OMIT_MICROSECONDS,
+    OPT_PASSTHROUGH_BIG_INT,
+    OPT_PASSTHROUGH_DATACLASS,
+    OPT_PASSTHROUGH_DATETIME,
+    OPT_PASSTHROUGH_SUBCLASS,
+    OPT_PASSTHROUGH_TUPLE,
+    OPT_SERIALIZE_NUMPY,
+    OPT_SERIALIZE_PYDANTIC,
+    OPT_SORT_KEYS,
+    OPT_UTC_Z,
+    Ext,
+    MsgpackDecodeError,
+    MsgpackEncodeError,
+    __version__,
+    packb,
+    unpackb,
+)
 
 __all__ = (
     "__version__",
@@ -13,7 +31,7 @@ __all__ = (
     "OPT_NAIVE_UTC",
     "OPT_NON_STR_KEYS",
     "OPT_OMIT_MICROSECONDS",
-    "OPT_PASSTHROUGH_BIGINT",
+    "OPT_PASSTHROUGH_BIG_INT",
     "OPT_PASSTHROUGH_DATACLASS",
     "OPT_PASSTHROUGH_DATETIME",
     "OPT_PASSTHROUGH_SUBCLASS",
