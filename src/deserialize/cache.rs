95eba3fad30b184424d8fb86de08ec6c799909fd
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2024-10-27 18:01:13 +0200

    Optimize deserialization of map keys
    This commit refactors the deserializer so as to not perform the
    expensive utf-8 validation for map string keys that are present in the
    cache, as it is redundant.
    
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- src/deserialize/cache.rs
+++ src/deserialize/cache.rs
@@ -1,48 +1,75 @@
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 use crate::typeref::*;
-use associative_cache::replacement::RoundRobinReplacement;
-use associative_cache::*;
+use crate::unicode::*;
 use once_cell::unsync::OnceCell;
+use simdutf8::basic::{from_utf8, Utf8Error};
 use std::hash::BuildHasher;
 use std::hash::Hasher;
-use std::os::raw::c_void;
+use std::ptr::NonNull;
 
 #[repr(transparent)]
-pub struct CachedKey {
-    ptr: *mut c_void,
+struct CachedKey {
+    ptr: *mut pyo3::ffi::PyObject,
 }
 
 unsafe impl Send for CachedKey {}
 unsafe impl Sync for CachedKey {}
 
 impl CachedKey {
-    pub fn new(ptr: *mut pyo3::ffi::PyObject) -> CachedKey {
-        CachedKey {
-            ptr: ptr as *mut c_void,
-        }
+    fn new(ptr: *mut pyo3::ffi::PyObject) -> CachedKey {
+        CachedKey { ptr: ptr }
     }
 
-    pub fn get(&mut self) -> *mut pyo3::ffi::PyObject {
-        let ptr = self.ptr as *mut pyo3::ffi::PyObject;
-        ffi!(Py_INCREF(ptr));
-        ptr
+    fn get(&mut self) -> *mut pyo3::ffi::PyObject {
+        ffi!(Py_INCREF(self.ptr));
+        self.ptr
     }
 }
 
 impl Drop for CachedKey {
     fn drop(&mut self) {
-        ffi!(Py_DECREF(self.ptr as *mut pyo3::ffi::PyObject));
+        ffi!(Py_DECREF(self.ptr));
     }
 }
 
-pub type KeyMap =
-    AssociativeCache<u64, CachedKey, Capacity512, HashDirectMapped, RoundRobinReplacement>;
-
-pub static mut KEY_MAP: OnceCell<KeyMap> = OnceCell::new();
-
-pub fn cache_hash(key: &[u8]) -> u64 {
-    let mut hasher = unsafe { HASH_BUILDER.get().unwrap().build_hasher() };
-    hasher.write(key);
-    hasher.finish()
+pub struct KeyMap<const C: usize> {
+    entries: Vec<Option<CachedKey>>,
 }
+
+impl<const C: usize> KeyMap<C> {
+    pub fn new() -> Self {
+        let mut entries = Vec::with_capacity(C);
+        for _ in 0..C {
+            entries.push(None);
+        }
+        KeyMap { entries: entries }
+    }
+
+    pub fn get(
+        &mut self,
+        key: &[u8],
+    ) -> std::result::Result<NonNull<pyo3::ffi::PyObject>, Utf8Error> {
+        let mut hasher = unsafe { HASH_BUILDER.get().unwrap().build_hasher() };
+        let hash = {
+            hasher.write(key);
+            hasher.finish()
+        } as usize;
+        let index = hash % C;
+        let entry = match &mut self.entries[index] {
+            Some(v) if unicode_to_str(v.ptr).unwrap().as_bytes() == key => v,
+            _ => {
+                let pykey = unicode_from_str(from_utf8(key)?);
+                hash_str(pykey);
+                self.entries[index] = Some(CachedKey::new(pykey));
+                match &mut self.entries[index] {
+                    Some(v) => v,
+                    _ => unreachable!(),
+                }
+            }
+        };
+        Ok(nonnull!(entry.get()))
+    }
+}
+
+pub static mut KEY_MAP: OnceCell<KeyMap<512>> = OnceCell::new();
