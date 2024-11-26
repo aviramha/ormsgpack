e390fdcb2f75009373cc3b3a5e07e80a0d244c1d
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2024-11-26 20:29:40 +0200

    Style fixes
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- src/serialize/serializer.rs
+++ src/serialize/serializer.rs
@@ -30,7 +30,7 @@ pub enum Error {
 
 impl std::fmt::Display for Error {
     #[cold]
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
             Error::Custom(ref msg) => f.write_str(msg),
             Error::Write => f.write_str("write error"),
