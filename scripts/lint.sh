2b957911677e4f9f85f73b4307551bd1eba60229
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2025-01-01 10:34:15 +0100

    Use uv to manage the project environment
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,8 +2,9 @@
 
 set -eou pipefail
 
-ruff format --check .
-ruff check .
-mypy .
+uv sync --frozen
+uv run --no-sync ruff format --check .
+uv run --no-sync ruff check .
+uv run --no-sync mypy .
 cargo fmt --check
 cargo clippy -- -D warnings
