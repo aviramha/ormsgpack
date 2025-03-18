e4791f8b06ad2b2d8edf8d7b056601089a0fbc97
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2025-03-18 19:49:09 +0200

    Test every MessagePack Ext format
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- tests/test_ext_type.py
+++ tests/test_ext_type.py
@@ -4,9 +4,21 @@ import pytest
 import ormsgpack
 
 
-def test_ext_type() -> None:
+@pytest.mark.parametrize(
+    "data",
+    (
+        pytest.param(b"a" * 1, id="fixext 1"),
+        pytest.param(b"a" * 2, id="fixext 2"),
+        pytest.param(b"a" * 4, id="fixext 4"),
+        pytest.param(b"a" * 8, id="fixext 8"),
+        pytest.param(b"a" * 16, id="fixext 16"),
+        pytest.param(b"a" * 32, id="ext 8"),
+        pytest.param(b"a" * 256, id="ext 16"),
+        pytest.param(b"a" * 65536, id="ext 32"),
+    ),
+)
+def test_ext_type(data: bytes) -> None:
     tag = 1
-    data = b"test"
     packed = ormsgpack.packb(ormsgpack.Ext(tag, data))
     assert packed == msgpack.packb(msgpack.ExtType(tag, data))
 
