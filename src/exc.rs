bd5994b6ffe1344d38b0430db634a83d5b7df893
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2025-03-22 10:01:53 +0200

    Move pydantic model serializer to a separate module
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- src/exc.rs
+++ src/exc.rs
@@ -3,4 +3,3 @@
 pub const INVALID_STR: &str = "str is not valid UTF-8: surrogates not allowed";
 pub const RECURSION_LIMIT_REACHED: &str = "Recursion limit reached";
 pub const KEY_MUST_BE_STR: &str = "Dict key must be str";
-pub const PYDANTIC_MUST_HAVE_DICT: &str = "Pydantic's BaseModel must have __dict__ attribute";
