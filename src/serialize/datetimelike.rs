9f11e6a7b1b16cbbea86412639a55579babb17e9
Emanuele Giaquinta <emanuele.giaquinta@gmail.com>
2025-05-11 23:41:20 +0300

    Optimize datetime and uuid serialization
    Serialize the value in an array wrapped in a cursor instead of in a
    SmallVec vector.
    
    Signed-off-by: Emanuele Giaquinta <emanuele.giaquinta@gmail.com>

--- src/serialize/datetimelike.rs
+++ src/serialize/datetimelike.rs
@@ -1,18 +1,22 @@
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 use crate::opt::*;
+use byteorder::WriteBytesExt;
 use chrono::{Datelike, Timelike};
 use serde::ser::{Serialize, Serializer};
 
-pub type DateTimeBuffer = smallvec::SmallVec<[u8; 32]>;
-
-fn write_integer(buf: &mut DateTimeBuffer, value: i32, width: usize) {
+fn write_integer<W>(writer: &mut W, value: i32, width: usize) -> Result<(), std::io::Error>
+where
+    W: std::io::Write,
+{
     let mut itoa_buf = itoa::Buffer::new();
     let formatted = itoa_buf.format(value);
     for _ in 0..width - formatted.len() {
-        buf.push(b'0');
+        writer.write_u8(b'0')?;
     }
-    buf.extend_from_slice(formatted.as_bytes());
+    let len = writer.write(formatted.as_bytes())?;
+    debug_assert!(len == formatted.len());
+    Ok(())
 }
 
 pub trait DateLike {
@@ -20,12 +24,16 @@ pub trait DateLike {
     fn month(&self) -> i32;
     fn day(&self) -> i32;
 
-    fn write_buf(&self, buf: &mut DateTimeBuffer) {
-        write_integer(buf, self.year(), 4);
-        buf.push(b'-');
-        write_integer(buf, self.month(), 2);
-        buf.push(b'-');
-        write_integer(buf, self.day(), 2);
+    fn write_buf<W>(&self, writer: &mut W) -> Result<(), std::io::Error>
+    where
+        W: std::io::Write,
+    {
+        write_integer(writer, self.year(), 4)?;
+        writer.write_u8(b'-')?;
+        write_integer(writer, self.month(), 2)?;
+        writer.write_u8(b'-')?;
+        write_integer(writer, self.day(), 2)?;
+        Ok(())
     }
 }
 
@@ -35,19 +43,23 @@ pub trait TimeLike {
     fn second(&self) -> i32;
     fn microsecond(&self) -> i32;
 
-    fn write_buf(&self, buf: &mut DateTimeBuffer, opts: Opt) {
-        write_integer(buf, self.hour(), 2);
-        buf.push(b':');
-        write_integer(buf, self.minute(), 2);
-        buf.push(b':');
-        write_integer(buf, self.second(), 2);
+    fn write_buf<W>(&self, writer: &mut W, opts: Opt) -> Result<(), std::io::Error>
+    where
+        W: std::io::Write,
+    {
+        write_integer(writer, self.hour(), 2)?;
+        writer.write_u8(b':')?;
+        write_integer(writer, self.minute(), 2)?;
+        writer.write_u8(b':')?;
+        write_integer(writer, self.second(), 2)?;
         if opts & OMIT_MICROSECONDS == 0 {
             let microsecond = self.microsecond();
             if microsecond != 0 {
-                buf.push(b'.');
-                write_integer(buf, microsecond, 6);
+                writer.write_u8(b'.')?;
+                write_integer(writer, microsecond, 6)?;
             }
         }
+        Ok(())
     }
 }
 
@@ -61,17 +73,22 @@ pub trait DateTimeLike: DateLike + TimeLike {
     fn has_tz(&self) -> bool;
     fn offset(&self) -> Offset;
 
-    fn write_buf(&self, buf: &mut DateTimeBuffer, opts: Opt) {
-        DateLike::write_buf(self, buf);
-        buf.push(b'T');
-        TimeLike::write_buf(self, buf, opts);
+    fn write_buf<W>(&self, writer: &mut W, opts: Opt) -> Result<(), std::io::Error>
+    where
+        W: std::io::Write,
+    {
+        DateLike::write_buf(self, writer)?;
+        writer.write_u8(b'T')?;
+        TimeLike::write_buf(self, writer, opts)?;
         if self.has_tz() || opts & NAIVE_UTC != 0 {
             let offset = self.offset();
             if offset.second == 0 {
                 if opts & UTC_Z != 0 {
-                    buf.push(b'Z');
+                    writer.write_u8(b'Z')?;
                 } else {
-                    buf.extend_from_slice(b"+00:00");
+                    let tz = b"+00:00";
+                    let len = writer.write(tz)?;
+                    debug_assert!(len == tz.len());
                 }
             } else {
                 let offset_hour: i32;
@@ -79,11 +96,11 @@ pub trait DateTimeLike: DateLike + TimeLike {
                 let mut offset_second: i32;
                 if offset.day == -1 {
                     // datetime.timedelta(days=-1, seconds=68400) -> -05:00
-                    buf.push(b'-');
+                    writer.write_u8(b'-')?;
                     offset_second = 86400 - offset.second;
                 } else {
                     // datetime.timedelta(seconds=37800) -> +10:30
-                    buf.push(b'+');
+                    writer.write_u8(b'+')?;
                     offset_second = offset.second;
                 }
                 (offset_minute, offset_second) = (offset_second / 60, offset_second % 60);
@@ -95,11 +112,12 @@ pub trait DateTimeLike: DateLike + TimeLike {
                 if offset_second >= 30 {
                     offset_minute += 1;
                 }
-                write_integer(buf, offset_hour, 2);
-                buf.push(b':');
-                write_integer(buf, offset_minute, 2);
+                write_integer(writer, offset_hour, 2)?;
+                writer.write_u8(b':')?;
+                write_integer(writer, offset_minute, 2)?;
             }
         }
+        Ok(())
     }
 }
 
@@ -155,8 +173,10 @@ impl Serialize for NaiveDateTime {
     where
         S: Serializer,
     {
-        let mut buf = DateTimeBuffer::new();
-        DateTimeLike::write_buf(self, &mut buf, self.opts);
-        serializer.serialize_str(str_from_slice!(buf.as_ptr(), buf.len()))
+        let mut cursor = std::io::Cursor::new([0u8; 32]);
+        DateTimeLike::write_buf(self, &mut cursor, self.opts).unwrap();
+        let len = cursor.position() as usize;
+        let value = unsafe { std::str::from_utf8_unchecked(&cursor.get_ref()[0..len]) };
+        serializer.serialize_str(value)
     }
 }
--- src/serialize/uuid.rs
+++ src/serialize/uuid.rs
@@ -1,63 +1,73 @@
 // SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 use crate::typeref::*;
+use byteorder::WriteBytesExt;
 use serde::ser::{Serialize, Serializer};
-use smallvec::SmallVec;
-use std::io::Write;
 use std::os::raw::c_uchar;
-
-pub type UUIDBuffer = smallvec::SmallVec<[u8; 64]>;
 
 pub struct UUID {
     ptr: *mut pyo3::ffi::PyObject,
+}
+
+const HEX: [u8; 16] = [
+    b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'a', b'b', b'c', b'd', b'e', b'f',
+];
+
+fn write_group<W>(writer: &mut W, group: &[c_uchar]) -> Result<(), std::io::Error>
+where
+    W: std::io::Write,
+{
+    for i in 0..group.len() {
+        writer.write_u8(HEX[(group[i] >> 4) as usize])?;
+        writer.write_u8(HEX[(group[i] & 0x0f) as usize])?;
+    }
+    Ok(())
 }
 
 impl UUID {
     pub fn new(ptr: *mut pyo3::ffi::PyObject) -> Self {
         UUID { ptr: ptr }
     }
-    pub fn write_buf(&self, buf: &mut UUIDBuffer) {
-        let value: u128;
-        {
-            // test_uuid_immutable, test_uuid_int
-            let py_int = ffi!(PyObject_GetAttr(self.ptr, INT_ATTR_STR));
-            ffi!(Py_DECREF(py_int));
-            let buffer: [c_uchar; 16] = [0; 16];
-            unsafe {
-                // test_uuid_overflow
-                pyo3::ffi::_PyLong_AsByteArray(
-                    py_int as *mut pyo3::ffi::PyLongObject,
-                    buffer.as_ptr() as *mut c_uchar,
-                    16,
-                    1, // little_endian
-                    0, // is_signed
-                )
-            };
-            value = u128::from_le_bytes(buffer);
-        }
+    pub fn write_buf<W>(&self, writer: &mut W) -> Result<(), std::io::Error>
+    where
+        W: std::io::Write,
+    {
+        let buffer: [c_uchar; 16] = [0; 16];
+        unsafe {
+            let value = pyo3::ffi::PyObject_GetAttr(self.ptr, INT_ATTR_STR);
+            pyo3::ffi::_PyLong_AsByteArray(
+                value as *mut pyo3::ffi::PyLongObject,
+                buffer.as_ptr() as *mut c_uchar,
+                16,
+                0, // little_endian
+                0, // is_signed
+            );
+            pyo3::ffi::Py_DECREF(value);
+        };
 
-        let mut hexadecimal: SmallVec<[u8; 32]> = SmallVec::with_capacity(32);
-        write!(hexadecimal, "{value:032x}").unwrap();
-
-        buf.extend_from_slice(&hexadecimal[..8]);
-        buf.push(b'-');
-        buf.extend_from_slice(&hexadecimal[8..12]);
-        buf.push(b'-');
-        buf.extend_from_slice(&hexadecimal[12..16]);
-        buf.push(b'-');
-        buf.extend_from_slice(&hexadecimal[16..20]);
-        buf.push(b'-');
-        buf.extend_from_slice(&hexadecimal[20..]);
+        write_group(writer, &buffer[..4])?;
+        writer.write_u8(b'-')?;
+        write_group(writer, &buffer[4..6])?;
+        writer.write_u8(b'-')?;
+        write_group(writer, &buffer[6..8])?;
+        writer.write_u8(b'-')?;
+        write_group(writer, &buffer[8..10])?;
+        writer.write_u8(b'-')?;
+        write_group(writer, &buffer[10..])?;
+        Ok(())
     }
 }
+
 impl Serialize for UUID {
     #[inline(never)]
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let mut buf: UUIDBuffer = smallvec::SmallVec::with_capacity(64);
-        self.write_buf(&mut buf);
-        serializer.serialize_str(str_from_slice!(buf.as_ptr(), buf.len()))
+        let mut cursor = std::io::Cursor::new([0u8; 64]);
+        self.write_buf(&mut cursor).unwrap();
+        let len = cursor.position() as usize;
+        let value = unsafe { std::str::from_utf8_unchecked(&cursor.get_ref()[0..len]) };
+        serializer.serialize_str(value)
     }
 }
